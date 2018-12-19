93f6d8b71b104415a32ecf8b40541baf7b7de95d
Vasiliy Hleb <vasilyhleb@ya.ru>
2018-12-19 11:49:30 -0800

    f-string in python 3.6+

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # testing
 This file is for test only.
 Maybe it will come out of something worthwhile:)
+
+
+f-string in python 3.6+ - it's cool!
---
+++ new_function.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.version)
+
+error = '123049123481-23x'
+name = 'Stansilav'
+familiya = 'Mujichok'
+full_name = name + familiya
+
+a = f'my name is {name.upper()}'
+print(f'error is {error}')
+print (a)
